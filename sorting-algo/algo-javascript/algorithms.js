e637c837dff43050aa4b37ca8f3eb2ffda0cb6a3
Jerome Luce <jerome.luce@protonmail.com>
2020-08-24 01:09:38 +0200

    Implement inversion sort

--- sorting-algo/algo-javascript/algorithms.js
+++ sorting-algo/algo-javascript/algorithms.js
@@ -30,3 +30,43 @@ function bubbleSort(array) {
 // console.log("Sorting array now...");
 // bubbleSort(trialArray);
 // console.log(trialArray);
+
+/* Onto Insertion Sort
+ Insertion Sort sorts the array in one pass by creating a new sorted array.
+ It takes the second item of an array, compares it to the first, and puts it either before or after. It then compares the 3rd item to the 2nd, and if needed to the first, and places it accordingly. So on and so forth until the whole array is sorted.
+*/
+
+function insertionSort(array) {
+  let sortedArray = [];
+  originalArrayLength = array.length
+  sortedArray.push(array.shift());
+  for (let i = 1; i < originalArrayLength; i++) {
+    let pointerValue = array.shift();
+    if (pointerValue >= sortedArray[(sortedArray.length - 1)]) {
+      sortedArray.push(pointerValue);
+    } else if (sortedArray.length === 1 && pointerValue < sortedArray[sortedArray.length -1]) {
+      sortedArray.unshift(pointerValue);
+    } else {
+      let inserted = false;
+      let temp = sortedArray.length;
+      for (let i2 = temp; i2 > 0; i2--) {
+        if (pointerValue >= sortedArray[i2 - 1]) {
+          sortedArray.splice(i2, 0, pointerValue);
+          inserted = true;
+          break;
+        };
+      };
+      if (inserted === false) {
+        sortedArray.unshift(pointerValue);
+      };
+    };
+
+  };
+  return sortedArray;
+};
+
+let trialArray = createRandomArray(100, 50);
+console.log(trialArray);
+console.log("Sorting array now...");
+let newar = insertionSort(trialArray);
+console.log(newar);
